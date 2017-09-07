ae9ec33dba3ba302be946d06120b30b2026898f1
Mauro Junior <mauropereirajr@gmail.com>
2017-09-07 12:51:46 -0300

    chore: building new structure

---
+++ src/compiler.js
@@ -0,0 +1,95 @@
+import * as path from 'path';
+import SingleEntryPlugin from 'webpack/lib/SingleEntryPlugin';
+
+export default function childCompiler(options, context, compilation) {
+  // The entry file is just an empty helper as the dynamic template
+  // require is added in "loader.js"
+  const outputOptions = {
+    filename: options.statsFilename,
+    publicPath: compilation.outputOptions.publicPath
+  };
+  // Create an additional child compiler which takes the template
+  // and turns it into an Node.JS html factory.
+  // This allows us to use loaders during the compilation
+  const compilerName = getCompilerName(context, outputOptions.filename);
+  const compiler = compilation.createChildCompiler(compilerName, outputOptions);
+  compiler.context = context;
+  compiler.apply(new SingleEntryPlugin(context, '!!' + require.resolve('./favicons.js') + '?' +
+      JSON.stringify({
+        outputFilePrefix: options.prefix,
+        icons: options.icons,
+        background: options.background,
+        persistentCache: options.persistentCache,
+        appName: options.title
+      }) + '!' + options.logo));
+
+  // Fix for "Uncaught TypeError: __webpack_require__(...) is not a function"
+  // Hot module replacement requires that every child compiler has its own
+  // cache. @see https://github.com/ampedandwired/html-webpack-plugin/pull/179
+  compiler.plugin('compilation', (compilation) => {
+    if (compilation.cache) {
+      if (!compilation.cache[compilerName]) {
+        compilation.cache[compilerName] = {};
+      }
+      compilation.cache = compilation.cache[compilerName];
+    }
+    compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+      if (!chunks[0]) {
+        return callback(compilation.errors[0] || 'Favicons generation failed');
+      }
+      const resultFile = chunks[0].files[0];
+      const resultCode = compilation.assets[resultFile].source();
+      let resultJson;
+      try {
+        /* eslint no-eval:0 */
+        const result = eval(resultCode);
+        resultJson = JSON.stringify(result);
+      } catch (e) {
+        return callback(e);
+      }
+      compilation.assets[resultFile] = {
+        source: () => resultJson,
+        size: () => resultJson.length
+      };
+      callback(null);
+    });
+  });
+
+  // Compile and return a promise
+  return new Promise((resolve, reject) => {
+    compiler.runAsChild((err, entries, childCompilation) => {
+      if (err) {
+        return reject(err);
+      }
+      // Replace [hash] placeholders in filename
+      const outputName = compilation.mainTemplate.applyPluginsWaterfall('asset-path', outputOptions.filename, {
+        hash: childCompilation.hash,
+        chunk: entries[0]
+      });
+      // Resolve / reject the promise
+      if (childCompilation && childCompilation.errors && childCompilation.errors.length) {
+        const errorDetails = childCompilation.errors
+          .map(error => error.message + (error.error ? ':\n' + error.error : ''))
+          .join('\n');
+
+        reject(new Error('Child compilation failed:\n' + errorDetails));
+      } else if (err) {
+        reject(err);
+      } else {
+        resolve({
+          outputName,
+          stats: JSON.parse(childCompilation.assets[outputName].source())
+        });
+      }
+    });
+  });
+};
+
+/**
+ * Returns the child compiler name e.g. 'html-webpack-plugin for "index.html"'
+ */
+function getCompilerName(context, filename) {
+  const absolutePath = path.resolve(context, filename);
+  const relativePath = path.relative(context, absolutePath);
+  return 'favicons-webpack-plugin for "' + (absolutePath.length < relativePath.length ? absolutePath : relativePath) + '"';
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,95 @@
-export default function hello() {
-  return 'Hello World.';
+import * as assert from 'assert';
+
+import childCompiler from './compiler';
+
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+
+function FaviconsWebpackPlugin(opts) {
+  let options = opts;
+
+  if (typeof opts === 'string') {
+    options = { logo: opts };
+  }
+  assert(typeof options === 'object', 'FaviconsWebpackPlugin options are required');
+  assert(options.logo, 'An input file is required');
+  this.options = _.extend({
+    prefix: 'icons-[hash]/',
+    emitStats: false,
+    statsFilename: 'iconstats-[hash].json',
+    persistentCache: true,
+    inject: true,
+    background: '#fff'
+  }, options);
+  this.options.icons = _.extend({
+    android: true,
+    appleIcon: true,
+    appleStartup: true,
+    coast: false,
+    favicons: true,
+    firefox: true,
+    opengraph: false,
+    twitter: false,
+    yandex: false,
+    windows: false
+  }, this.options.icons);
 }
+
+FaviconsWebpackPlugin.prototype.apply = function apply(compiler) {
+  const self = this;
+
+  if (!self.options.title) {
+    self.options.title = guessAppName(compiler.context);
+  }
+
+  // Generate the favicons
+  let compilationResult;
+
+  compiler.plugin('make', (compilation, callback) => {
+    childCompiler.compileTemplate(self.options, compiler.context, compilation)
+      .then((result) => {
+        compilationResult = result;
+        callback();
+      })
+      .catch(callback);
+  });
+
+  // Hook into the html-webpack-plugin processing
+  // and add the html
+  if (self.options.inject) {
+    compiler.plugin('compilation', (compilation) => {
+      compilation.plugin('html-webpack-plugin-before-html-processing', (htmlPluginData, callback) => {
+        if (htmlPluginData.plugin.options.favicons !== false) {
+          htmlPluginData.html = htmlPluginData.html.replace(
+            /(<\/head>)/i, compilationResult.stats.html.join('') + '$&');
+        }
+        callback(null, htmlPluginData);
+      });
+    });
+  }
+
+  // Remove the stats from the output if they are not required
+  if (!self.options.emitStats) {
+    compiler.plugin('emit', (compilation, callback) => {
+      delete compilation.assets[compilationResult.outputName];
+      callback();
+    });
+  }
+};
+
+/**
+ * Tries to guess the name from the package.json
+ */
+function guessAppName(compilerWorkingDirectory) {
+  let packageJson = path.resolve(compilerWorkingDirectory, 'package.json');
+  if (!fs.existsSync(packageJson)) {
+    packageJson = path.resolve(compilerWorkingDirectory, '../package.json');
+    if (!fs.existsSync(packageJson)) {
+      return 'Webpack App';
+    }
+  }
+  return JSON.parse(fs.readFileSync(packageJson)).name;
+}
+
+module.exports = FaviconsWebpackPlugin;
